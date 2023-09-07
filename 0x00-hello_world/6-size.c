38b53f140125db657bd8a5593ecf1c434c0d499b
survivor017 <najisawny47@gmail.com>
2023-09-07 08:00:22 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 /**
  * main - A pragrame that print size
- * Return (0)
+ *
+ * Return: Always 0 (success)
+ *
  */  
 
 int main(void)
@@ -11,7 +13,6 @@ printf("Size of an int: %lu byte(s)\n", sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 printf("Size of a float: %lu byte(s)\n", sizeof(float));
-
-return (0);
+return 0;
 }
 
