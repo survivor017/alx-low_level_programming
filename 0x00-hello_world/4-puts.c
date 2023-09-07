5362e6d09fc654ad2b451237186e9626590b16ac
survivor017 <najisawny47@gmail.com>
2023-09-07 06:31:57 -0700

    mjo1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char str [] = "Programming is like building a multilingual puzzle";
-	puts(str);
+	puts("Programming is like building a multilingual puzzle");
+	
 	return (0);
 }
