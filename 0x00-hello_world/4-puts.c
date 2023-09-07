fd198e73b7a76d9d9f0c848768f6664c086fff95
survivor017 <najisawny47@gmail.com>
2023-09-07 06:36:51 -0700

    mjo13

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.n>
+#include <stdlib.h>
 /**
  * main - A pragram that print a line with puts fanctio
  *
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle")i;
 	
 	return (0);
 }
