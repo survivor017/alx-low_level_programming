aafe49bc8367e496768e2273ad8f94e9c7f4f72c
survivor017 <najisawny47@gmail.com>
2023-09-07 07:19:52 -0700

    mjo19

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- * main
+ * main - A progaram that print a line with puts fanctoin
  *
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
 }
