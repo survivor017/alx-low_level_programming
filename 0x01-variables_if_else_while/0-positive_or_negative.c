c0c25d3cadc04ff61da2994ab6119df64e2bfdcd
survivor017 <najisawny47@gmail.com>
2023-09-08 09:07:45 -0700

    positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,15 +12,17 @@ srand(time(0));
 
 n = rand() - RAND_MAX / 2;
 if (n > 0) 
-	
+{	
 printf("is positive". n);
+}
 
 else if (n == 0)
-	
+}	
 printf("is zero". n);
- 
-else 
+{
 
+else 
+{
 printf("is negative". n);
 }
 
