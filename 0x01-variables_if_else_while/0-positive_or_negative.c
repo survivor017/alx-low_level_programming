f2af968c13f58c188a0c137cab0111ecbbc50f47
survivor017 <najisawny47@gmail.com>
2023-09-08 06:42:30 -0700

    pn

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,33 +6,27 @@
  *
  * Return: Always 0 (success)
  */
-int main() 
+int main(void) 
 {	
 int n;
 
-// Initialize the random number generator with the current time
 srand(time(0));
 
-// Generate a random number and store it in n
-
 n = rand() - RAND_MAX / 2;
-
-// Check if the number is positive, zero, or negative
 if (n > 0) 
 {	
-printf("%d is positive\n". n);
+printf("is positive". n);
 }
 {
 else if (n == 0)
 }	
-printf("%d is zero\n". n);
+printf("is zero". n);
 } 
 else 
 {
-printf("%d is negative\n". n);
+printf("is negative". n);
 }
 
-// Print a new line
 printf("\n");
 
 return (0);
