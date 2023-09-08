bab87acfdc69251382ca3903fa3f9c7b485fb368
survivor017 <najisawny47@gmail.com>
2023-09-08 02:30:17 -0700

    p or n

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,15 @@
+#include <sdlib.h>
+#include <time.h>
+/***
+ *
+ *main programe that gevs numbre eche time
+ *
+ *betty style
+ ***/
+ lnt main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+return (0);
+}
