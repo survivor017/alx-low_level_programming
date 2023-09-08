1f349da84e68876a407df971a6e6afea38f81c60
survivor017 <najisawny47@gmail.com>
2023-09-08 03:12:26 -0700

    po or ne

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
+/*
+ *
+ * main that program gives number p or n
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+return (0);
+}
