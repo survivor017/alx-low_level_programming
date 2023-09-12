5313c6e90beb24470fc4424b603f0a9bae37cfd9
survivor017 <najisawny47@gmail.com>
2023-09-12 07:44:18 -0700

    hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@ i#include "main.h"
 
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer
- * starting from 00:00 to 23:59
+ * Starting from 00:00 to 23:59
  */
 void jack_bauer(void)
 {
